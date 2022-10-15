c92af9cbb856880f62e64a88736c1ddbc0517027
Segev Eliezer <77868212+0xd4y@users.noreply.github.com>
2022-10-14 22:37:15 -0500

    Update AWSealion.py

--- AWSealion.py
+++ AWSealion.py
@@ -4,7 +4,7 @@
 # Created by: Segev Eliezer
 # LinkedIn: https://www.linkedin.com/in/segev-eliezer/
 # This tool is made to help you keep stealthy, organized, and efficient during AWS engagements.
-# Date: September 10, 2022
+# Date: October 14, 2022
 ###############################################################################################
 
 import signal
@@ -22,6 +22,17 @@ tool_arguments = ['--all-regions', '--regions'] # Could potentially be deprecate
 
 command_arguments = sys.argv[1::1]
 
+# Command before try-block manipulation
+command_temp = ' '.join(command_arguments)
+
+## Puts help text in paginator just like actual aws binary
+if 'help' in command_arguments:
+    help_index = command_arguments.index('help')
+    if '--' != command_arguments[help_index - 1]:
+        command = 'aws ' + command_temp
+        os.system(command + ' 2>/dev/null')
+        sys.exit()
+
 # So that the first two arguments are the service and the argument. This avoids breaking the directory and file structure.
 try:
     while '-' == command_arguments[0][0]:
@@ -36,7 +47,7 @@ try:
     command_arguments.append(profile_argument)
     command_arguments.append(profile)
 except Exception as e:
-    ## If no profile is specified, use the default profile, but ignore this if the user just wants to configure things 
+    ## If no profile is specified, use the default profile, but ignore this if the user just wants to configure things
     if len(command_arguments) > 0 and 'sealion' not in command_arguments:
         profile = 'default'
         command_arguments.append('--profile')
@@ -50,15 +61,6 @@ command = ' '.join(command_arguments)
 if len(command_arguments) == 2:
     if 'configure' == command_arguments[0] or 'configure' == command_arguments[1]:
         os.system('aws ' + command)
-        sys.exit()
-
-
-## Puts help text in paginator just like actual aws binary
-if 'help' in command_arguments:
-    help_index = command_arguments.index('help')
-    if '--' != command_arguments[help_index - 1]:
-        command = 'aws ' + command
-        os.system(command)
         sys.exit()
 
 sealion_path = os.getenv('HOME') + '/.awsealion/'
@@ -130,7 +132,7 @@ def pentest_user_agent(user_agent):
             confirmation = confirmation[0].strip()
             pen_user_agent.close()
         if len(user_agent) == 0 and len(default_user_agent.strip()) == 0:
-            if any(bad_user_agent in boto3_user_agent for bad_user_agent in bad_user_agents) and confirmation != 'y': 
+            if any(bad_user_agent in boto3_user_agent for bad_user_agent in bad_user_agents) and confirmation != 'y':
                 cprint('[x] Detected pentesting distro user agent. \nChange this with \'aws sealion --set-user-agent ' + current_engagement + ' "aws-cli/1.16.145 Python/3.6.7 Linux/4.15.0-45-generic botocore/1.12.168"\' or any user agent of your choice.\n', 'red')
                 return True
 
@@ -138,7 +140,7 @@ def pentest_user_agent(user_agent):
         if len(user_agent) == 0 and len(default_user_agent.strip()) == 0:
             user_agent = default_user_agent
             confirmation = ''
-            if any(bad_user_agent in boto3_user_agent for bad_user_agent in bad_user_agents): 
+            if any(bad_user_agent in boto3_user_agent for bad_user_agent in bad_user_agents):
                 cprint('[x] Detected pentesting distro user agent. \nChange this with \'aws sealion --set-user-agent ' + current_engagement + ' "aws-cli/1.16.145 Python/3.6.7 Linux/4.15.0-45-generic botocore/1.12.168"\' or any user agent of your choice.\n', 'red')
                 confirmation = input('Are you sure you want to proceed? Type y to proceed and ignore these warnings, w to proceed and continue receiving these warnings, or n to cancel the API call. [y|w|n]: ')
                 if confirmation == 'y' or confirmation == 'w':
@@ -154,7 +156,7 @@ def pentest_user_agent(user_agent):
 
 pentest_user_agent_bool = pentest_user_agent(user_agent) # Check if pentest distro is being used
 
-# Master agent file is read by the botocore library. 
+# Master agent file is read by the botocore library.
 with open(sealion_path + 'user_agent.txt','w') as master_user_agent_file:
     if not pentest_user_agent_bool and len(user_agent) > 0:
         master_user_agent_file.write(user_agent)
@@ -207,7 +209,7 @@ def error(error):
     if error == "no_region" or region == '--profile': ## when no region is specified, the region may be --profile
         sys.exit("You have not inputted a value for --regions")
     elif error == "invalid_region":
-        sys.exit(f'''You have inputted {region} which is not a valid region. The following are valid regions:\n 
+        sys.exit(f'''You have inputted {region} which is not a valid region. The following are valid regions:\n
         us
         eu
         ap
@@ -255,10 +257,6 @@ elif '--region' in command_arguments:
         if confirmation != 'y':
             sys.exit()
 
-
-command = ' '.join(command_arguments)
-
-
 # Checks if a command was already executed. Prints the output of the command from a local file if it was already executed.
 def already_executed(command):
     if '--force' in command_arguments:
@@ -278,7 +276,7 @@ def already_executed(command):
             ## Sets were used to make sure two commands are the same regardless of the order of the arguments
             command_history = json.load(fj)
             for key, value in command_history.items():
-                key_temp = key + ' --region ' + profile_default_region 
+                key_temp = key + ' --region ' + profile_default_region
                 if command == key or same_command_test == set(key.split()) or command[4:] == key or same_command_test == set(key_temp.split()): # [4:] beause the first four characters are aws[:space:]
                     print('{\n    "AlreadyExecutedCommand": "'+command+'"\n}\n')
                     print(value)
@@ -296,7 +294,7 @@ def write_command(command, command_output):
         with open(json_file,"r") as fjr:
             history = json.load(fjr)
     except Exception as e:
-        
+       
         FileNotFound = True
         pass
 
@@ -332,14 +330,14 @@ def aws_execute(region):
             aws_error = aws_error.decode('utf-8')
 
             ## If the command is invalid
-            if 'To see help text, you can run:' in aws_error or 'help' in command_arguments or 'You must specify a region' in aws_error or 'Unknown output type:' in aws_error or 'Unable to locate credentials.' in aws_error:
+            if 'To see help text, you can run:' in aws_error or 'You must specify a region' in aws_error or 'Unknown output type:' in aws_error or 'Unable to locate credentials.' in aws_error:
                 command_output = aws_error
 
             ## If the command is valid, but there is some error like unauthorized
-            elif len(aws_error) > 0 and 'To see help text, you can run:' not in aws_error and 'help' not in command_arguments:
+            elif len(aws_error) > 0 and 'To see help text, you can run:' not in aws_error:
                 command_output = aws_error
                 write_command(command,command_output)
-            elif 'help' not in command_arguments:
+            else:
                 command_output = command_output.decode('utf-8')
                 write_command(command,command_output)
             print(command_output)
@@ -423,12 +421,12 @@ if not any(command_argument in tool_arguments for command_argument in command_ar
     else:
         os.system(command)
         sys.exit()
-    if 'To see help text, you can run:' in aws_error or 'help' in command_arguments or 'You must specify a region' in aws_error or 'Unknown output type:' in aws_error or 'Unable to locate credentials.' in aws_error:
+    if 'To see help text, you can run:' in aws_error or 'You must specify a region' in aws_error or 'Unknown output type:' in aws_error or 'Unable to locate credentials.' in aws_error:
         command_output = aws_error
-    elif len(aws_error) > 0 and 'aws: error:' not in aws_error and 'help' not in command_arguments:
+    elif len(aws_error) > 0 and 'aws: error:' not in aws_error:
         command_output = aws_error
         write_command(command,command_output)
-    elif 'help' not in command and len(aws_error) == 0:
+    elif len(aws_error) == 0:
         write_command(command,command_output)
     print(command_output)
 
